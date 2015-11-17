519991920d106a6d35eb7176cb7f9c178f4f9587
Chris LeBlanc <chris.leblanc@buzznog.com>
2015-11-16 18:31:53 -0600

    Fixing tabs.

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -3,10 +3,10 @@ import Immutable from 'immutable';
 const defaultState = new Immutable.List();
 
 export default function authReducer(state = defaultState, action) {
-  switch(action.type) {
-    case 'LOGIN':
-      return state.concat(action.res.data);
-    default:
-      return state;
-  }
+	switch(action.type) {
+		case 'LOGIN':
+		return state.concat(action.res.data);
+		default:
+		return state;
+	}
 }
