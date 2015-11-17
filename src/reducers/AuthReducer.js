f64bc8d9591e08420f915f9128e0d01922472f86
Chris LeBlanc <chris.leblanc@buzznog.com>
2015-11-17 12:46:41 -0600

    Adding auth to state.

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -5,8 +5,12 @@ const defaultState = new Immutable.List();
 export default function authReducer(state = defaultState, action) {
 	switch(action.type) {
 		case 'LOGIN_SUCCESS':
-		return state.concat(action.res.data);
+			return {
+				...state,
+				jwt: action.res.data.jwt,
+				user: action.res
+			};
 		default:
-		return state;
+			return state;
 	}
 }
