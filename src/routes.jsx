c7ca131974ad867e6837f097dca3b7c28f4121ba
Chris LeBlanc <chris.leblanc@buzznog.com>
2015-11-18 13:28:49 -0600

    Implement requireAuth route helper.

--- src/routes.jsx
+++ src/routes.jsx
@@ -12,9 +12,9 @@ export default function getRoutes(state) {
 
 	return (
 		<Route path="/" name="app" component={App}>
-			<IndexRoute component={Pages.Dashboard} />
-			<Route path="login" name="login" component={Pages.Login} />
-			<Route path="signup" name="signup" component={Pages.SignUp} />
+			<IndexRoute component={Pages.Dashboard} onEnter={requireAuth}/>
+			<Route path="login" name="login" component={Pages.Login}/>
+			<Route path="signup" name="signup" component={Pages.SignUp}/>
 		</Route>
 	);
 }
