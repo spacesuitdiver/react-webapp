af6eee3289827ef6fe1cbdf1e0557bb9952d08ab
Chris LeBlanc <chris.leblanc@buzznog.com>
2015-11-18 13:57:28 -0600

    Awkward...

--- src/components/pages/Login/Login.jsx
+++ src/components/pages/Login/Login.jsx
@@ -1,5 +1,4 @@
 import React  from 'react';
-import { PropTypes } from 'react-router';
 import { bindActionCreators } from 'redux';
 import { connect }            from 'react-redux';
 import { LoginForm } from 'components/molecules';
@@ -7,10 +6,6 @@ import { Dashhead } from 'components/atoms';
 import { LoginWithFacebookButton } from 'components/atoms';
 
 export default class Login extends React.Component {
-
-  static contextTypes = {
-    history: PropTypes.history
-  }
 
   render() {
     require("./Login.less");
@@ -20,7 +15,6 @@ export default class Login extends React.Component {
         <Dashhead title="Login" subTitle="Type in yo creds" />
         <LoginWithFacebookButton text="Login with Facebook" type="button"/>
         <LoginForm/>
-        <button onClick={() => this.context.history.replaceState(null, '/')}>Foo</button>
       </div>
     );
   }
