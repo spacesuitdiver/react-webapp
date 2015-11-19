8722d9f55570ec1efc9c23b8d0c2321df9c7fbd0
Zach Gibson <zach.gibson@buzznog.com>
2015-11-18 18:50:44 -0600

    Abstracting out text for hr component.

--- src/components/atoms/DividedHeading/DividedHeading.jsx
+++ src/components/atoms/DividedHeading/DividedHeading.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 export default class DividedHeading extends React.Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired
+  }
 
 render() {
     return (
       <div className="hr-divider">
-        <h3 className="hr-divider-content hr-divider-heading">
-          YAS
-        </h3>
+        <h3 className="hr-divider-content hr-divider-heading">{this.props.text}</h3>
       </div>
     );
   }
