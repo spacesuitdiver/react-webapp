a7b6d26f59a16b390e583015afeea3174eea5190
Chris LeBlanc <chris.leblanc@buzznog.com>
2015-11-25 16:37:00 -0600

    Removing subscriber abstraction.

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -3,14 +3,17 @@ import { createStore as _createStore,
          applyMiddleware }  from 'redux';
 import * as reducers      from 'reducers';
 import * as middlewares   from 'middlewares';
-import * as subscribers   from 'subscribers';
 
 const reducer = combineReducers(reducers);
 const middleware = [ middlewares.promise ];
 
 export default function createStore(initialState) {
   const store = applyMiddleware(...middleware)(_createStore)(reducer, initialState);
-  store.subscribe(subscribers.loggedIn(store));
+  store.subscribe(() => {
+  	const state = store.getState();
+  	if (state.auth.isLoggedIn 
+  		&& state.routing.path == "/login") store.dispatch(updatePath('/'));
+  });
 
   return store;
 }
--- src/subscribers/loggedInSubscriber.js
+++
@@ -1,7 +0,0 @@
-import { updatePath } from 'redux-simple-router';
-
-export default function loggedInSubscriber(store) {
-	const state = store.getState();
-	console.log(state);
-	if (state.auth.isLoggedIn && state.routing.path == "/login") store.dispatch(updatePath('/'))
-}
