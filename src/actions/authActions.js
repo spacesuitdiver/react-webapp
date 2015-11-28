05e23a986d01f65b107cc2628865b2ca19a3881d
Chris LeBlanc <chris.leblanc@buzznog.com>
2015-11-27 20:51:57 -0600

    Fix auth action creator.

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -5,13 +5,13 @@ const API_URL = 'https://api-v1.buzznog.com/auth/local';
 export function login(credentials) {
   return {
     type: 'LOGIN',
-    promise: request.post(API_URL, credentials)
+    request: request.post(API_URL, credentials)
   }
 }
 
 export function persistToken(token) {
   return {
     type: 'PERSIST_TOKEN',
-    persist: { token: token }
+    persist: { token: { token: token } }
   }
 }
