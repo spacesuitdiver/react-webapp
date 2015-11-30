77de9dc61c155e21d4dc7e631f38d78b5bb6ea23
Chris LeBlanc <chris.leblanc@buzznog.com>
2015-11-30 12:30:51 -0600

    Renaming promise middleware.

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -5,9 +5,9 @@ const API_URL = 'https://api-v1.buzznog.com/apps';
 export function getApps() {
 	return {
 		type: 'GET_APPS',
-		promise: request.get(API_URL, { 
+		request: request.get(API_URL, { 
 			headers: { 
-				'Authorization': 'Bearer ' + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImlkIjo3LCJyb2xlIjoiYWRtaW4ifSwiaWF0IjoxNDQ4NDkyMDkyLCJleHAiOjE0NDg1Nzg0OTIsImF1ZCI6ImJ1enpub2cuY29tIiwiaXNzIjoiYnV6em5vZy5jb20ifQ.w2soRQya3qV6odWp7X2YqCo_BDsL58RjSrDEchGmaK8"
+				'Authorization': 'Bearer ' + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImlkIjo3LCJyb2xlIjoiYWRtaW4ifSwiaWF0IjoxNDQ4NzUwMDAzLCJleHAiOjE0NDg4MzY0MDMsImF1ZCI6ImJ1enpub2cuY29tIiwiaXNzIjoiYnV6em5vZy5jb20ifQ.Si1n_RVrGBquWRbwn2zVi38MzFlv02pzRcGK-EaAOEc"
 			}
 		})
 	}
